09282dbc6671af5fad5646a8682a5cc7adb9fa88
rivas.fujos20 <rivas.fujos20@triana.salesianos.edu>
2021-12-02 09:25:58 +0100

    Un bucle en condiciones

--- src/com/salesianostriana/dam/Main.java
+++ src/com/salesianostriana/dam/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola");
         System.out.println("Hello World");
+
+
+        int[] arr = new int[] {123, 456, 789};
+
+        for (int a: arr) {
+            System.out.println(a);
+        }
     }
 }
