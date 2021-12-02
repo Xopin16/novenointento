54ce01c9feb9d0575a275c3e820400c8e1e035eb
rivas.fujos20 <rivas.fujos20@triana.salesianos.edu>
2021-12-02 09:16:41 +0100

    El trabajo de mi amigo blas

--- src/com/salesianostriana/dam/Main.java
+++ src/com/salesianostriana/dam/Main.java
@@ -6,5 +6,14 @@ public class Main {
 	// write your code here
         System.out.println("Hola");
         System.out.println("Hello World");
+
+        int [] array = new int [3];
+        array [0] =  123;
+        array [1] = 456;
+        array [2] = 789;
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
     }
 }
